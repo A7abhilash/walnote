b2750045355897f037b9f37cdf151ea3f043e251
A7abhilash <a7abhilash@gmail.com>
2020-11-16 21:14:38 +0530

    First commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import firebase from "firebase";
+import { config } from "./config.json";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB1v5p7NhrNxY8E9TERzh7VTmhjbt15ruM",
-    authDomain: "walnote-dev.firebaseapp.com",
-    databaseURL: "https://walnote-dev.firebaseio.com",
-    projectId: "walnote-dev",
-    storageBucket: "walnote-dev.appspot.com",
-    messagingSenderId: "467712672241",
-    appId: "1:467712672241:web:f7a084163ff07344e6c450"
-};
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
